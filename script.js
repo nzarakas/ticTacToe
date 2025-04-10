937ef9de6af0d4a0eaf69645fd5b121ddc87cf7b
Nicolas Zarakas <nzarakas1@gmail.com>
2025-04-10 17:55:06 +0300

    test

--- script.js
+++ script.js
@@ -26,24 +26,86 @@ function Gameboard(){
 
     return {getBoard, printBoard};
 }
-//testing shit
-const testBoard = Gameboard(); 
 
-testBoard.printBoard();
-
-console.log(testBoard.getBoard());
-
+//controls what's inside each cell of the array
 function Cell(){
     let mark = ' ';
 
     const getMark = () => mark;
 
-    return {getMark};
+    function setMark(playerMark) {
+        if (mark === ' '){
+            mark = playerMark;  
+        }
+    }
+
+    return {getMark, setMark};
+}
+//delete below test
+
+const testBoard = Gameboard(); 
+testBoard.printBoard();
+console.log(testBoard.getBoard());
+console.log(typeof testBoard);
+let PlayerOne = {
+    name: 'PlayerOneName',
+    mark: 'X',
+    score: 0
+}
+// testBoard[2].setMark(PlayerOne.mark);
+//until here
+
+
+
+function GameController(
+    PlayerOneName = 'Player One',
+    PlayerTwoName = 'Player Two'
+){
+    PlayerOne = {
+        name: PlayerOneName,
+        mark: 'X',
+        score: 0
+    }
+
+    PlayerTwo = {
+        name: PlayerTwoName,
+        mark: 'O',
+        score: 0
+    }
+
+    const board = Gameboard();
+
+    let currentPlayer = PlayerOne;
+
+    function playRound(){
+        //check if a victory condition has been achieved.(3 in a row anywhere?)
+            // if no victory achieved, run function that checks if there are any valid moves left.(is the board full?)
+            //Declare Tie if no victory but board is full.
+        
+        // run a function that switches active player? (this is probably declared in GameController)
+        function switchPlayer(){
+            if (currentPlayer === PlayerOne){
+                currentPlayer = PlayerTwo;
+                
+            }
+            else{
+                currentPlayer = PlayerOne;
+            }
+        }
+        // run a function that places a mark in a cell that the active player selected for this round. 
+        // (where is this function declared?) - maybe this function exists here and can be analyzed into two
+        // subfunctions:
+            // function that takes user input regarding which row and which column they choose 
+            // e.g playerOption in Cell() -> r=2, c=1. (Two functions?)
+            // board[2][1] 
+        function setPlayerMark(){
+            const row = 1;
+            const column = 2; 
+
+            
+        }
+        //printboard at the end of the round 
+    }
 }
 
-function Player(name = 'test', mark){
-    let score = 0 ;
-    
 
-//    return {name, mark, and a buch of other functions};
-}
